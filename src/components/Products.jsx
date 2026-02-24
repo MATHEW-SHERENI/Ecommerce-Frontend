1566eb54e5cca3a48f1ed38c622e0a4c4c1791c4
mudabs <mudaburamunashe@gmail.com>
2026-02-23 19:31:07 -0600

    First Commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,43 @@
+const Products = () => {
+    const isLoading = false;
+    const errorMessage = "";
+    
+    const products = [
+{
+      productId: 652,
+      productName: "Iphone Xs max",
+      image: "https://placehold.co/600x400",
+      description: "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
+      quantity: 0,
+      price: 1450.0,
+      discount: 10.0,
+      specialPrice: 1305.0,
+    },
+    {
+      productId: 654,
+      productName: "MacBook Air M2s",
+      image: "https://placehold.co/600x400",
+      description: "Ultra-thin laptop with Apple's M2 chip, providing fast performance in a lightweight, portable design.",
+      quantity: 0,
+      price: 2550.0,
+      discount: 20.0,
+      specialPrice: 2040.0,
+    }
+]
+    return(
+        <div className="lg:px-14 sm:px-8 px-4 2xl:w-[90%] 2xl:mx-auto">
+            {/* This checks if loading, else errorMessage else displays the products */}
+            {isLoading ? (
+                <p>It is Loading...</p>
+            ) : errorMessage ? (
+                <div><p>Error Message</p></div>
+            ) : (
+                <div className="min-h-175">
+                    <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+                        <p>Products</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
